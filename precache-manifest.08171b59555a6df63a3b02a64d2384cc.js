517a012ca5105d3644e3758332e16b7e7079b277
mario96541 <41206432+mario96541@users.noreply.github.com>
2019-08-09 16:01:21 +0300

    Updates

---
+++ precache-manifest.08171b59555a6df63a3b02a64d2384cc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13bb7358e26999ef457ba74634cbec74",
+    "url": "/react-hw-4-pet-shop/index.html"
+  },
+  {
+    "revision": "023dbe224daf9d05782e",
+    "url": "/react-hw-4-pet-shop/static/css/main.c8e6f6cc.chunk.css"
+  },
+  {
+    "revision": "e9c0fe11fe78777f59d0",
+    "url": "/react-hw-4-pet-shop/static/js/2.849dec4f.chunk.js"
+  },
+  {
+    "revision": "023dbe224daf9d05782e",
+    "url": "/react-hw-4-pet-shop/static/js/main.70760a99.chunk.js"
+  },
+  {
+    "revision": "43b8743bbc9dabfea9e7",
+    "url": "/react-hw-4-pet-shop/static/js/runtime~main.5e4f2825.js"
+  }
+]);
